dc23c89bc9b3ccc7b7aa6b8178ba62210f72c35d
Yoad.Matza <yoadmatza5@gmail.com>
2021-10-07 16:40:33 +0300

    dor finaly

--- python_summery/app.py
+++ python_summery/app.py
@@ -17,6 +17,7 @@ def get_location():
     except:
         return 'Jerusalem'
 
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     config = configparser.ConfigParser()
@@ -29,29 +30,78 @@ def home():
         city_name = get_location()
     daily_source = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&units=metric&appid={API_KEY}'
     daily_data = requests.get(daily_source).json()
-    print(daily_data)
+
+    exception_dict = {400: "Please insert a city name", 401: "API key error", 404: "City not found",
+                            429: "Too many requests"}
+
     now = datetime.now()  # current date and time
-    date_time = now.strftime("%d-%m-%Y, %H:%M")
+    date_time = now.strftime("%d/%m/%Y, %H:%M")
 
     class DailyWeather:
-        city = daily_data['name']
-        country = daily_data['sys']['country']
-        temp = "{:.1f}".format(int(daily_data['main']['temp']))
-        temp_feels = "{:.1f}".format(int(daily_data['main']['feels_like']))
-        wind = "{:.1f}".format(daily_data['wind']['speed'] * 3.6)
-        humidity = daily_data['main']['humidity']
-        temp_max = "{:.1f}".format(int(daily_data['main']['temp_max']))
-        temp_min = "{:.1f}".format(int(daily_data['main']['temp_min']))
-        city_lon = daily_data['coord']['lon']
-        city_lat = daily_data['coord']['lat']
-        datetime_get = daily_data['dt']
-        short_desc = daily_data['weather'][0]['main']
-        desc = daily_data['weather'][0]['description']
-        icon_data = daily_data['weather'][0]['icon']
-        icon = f'http://openweathermap.org/img/w/{icon_data}.png'
+        if daily_data['cod'] == 200:
+            city = daily_data['name']
+            country = daily_data['sys']['country']
+            temp = "{:.1f}".format(int(daily_data['main']['temp']))
+            temp_feels = "{:.1f}".format(int(daily_data['main']['feels_like']))
+            wind = "{:.1f}".format(daily_data['wind']['speed'] * 3.6)
+            humidity = daily_data['main']['humidity']
+            temp_max = "{:.1f}".format(int(daily_data['main']['temp_max']))
+            temp_min = "{:.1f}".format(int(daily_data['main']['temp_min']))
+            city_lon = daily_data['coord']['lon']
+            city_lat = daily_data['coord']['lat']
+            datetime_get = daily_data['dt']
+            short_desc = daily_data['weather'][0]['main']
+            desc = daily_data['weather'][0]['description']
+            icon_data = daily_data['weather'][0]['icon']
+            icon = f'http://openweathermap.org/img/w/{icon_data}.png'
 
-    weekly_source = f'https://api.openweathermap.org/data/2.5/onecall?lat={DailyWeather.city_lat}&lon={DailyWeather.city_lon}&units=metric&exclude=current,minutely,hourly,alerts&appid={API_KEY}'
+    if daily_data['cod'] == 200:
+        weekly_source = f'https://api.openweathermap.org/data/2.5/onecall?lat' \
+                        f'={DailyWeather.city_lat}&lon={DailyWeather.city_lon}&units=metric&exclude=current,minutely,hourly,alerts&appid={API_KEY}'
+        weekly_data = requests.get(weekly_source).json()
+
+        s = str(now.strftime("%d/%m/%Y"))
+        print(s)
+        date = datetime.strptime(s, '%d/%m/%Y').date()
+        print(date)
+        weather_data = {}
+        i = 0
+        while i <= 7:
+            modified_date = date + timedelta(days=i)
+            datetime.strftime(modified_date, '%d/%m/%Y')
+            dict1 = {i: {'day': weekly_data['daily'][i]['temp']['day'], 'night': weekly_data['daily'][i]['temp']['night'],
+                         'weather': weekly_data['daily'][i]['weather'][0]['description'],
+                         'humidity': weekly_data['daily'][i]['humidity'], 'date': modified_date},
+                     }
+            weather_data.update(dict1)
+            i += 1
+        print(weather_data)
+        return render_template('home.html', date_time=date_time, DailyWeather=DailyWeather, WeeklyWeather=weather_data)
+
+    return error_handler(daily_data, API_KEY, exception_dict, now, date_time)
+
+@app.route('/error')
+def error_handler(daily_data, API_KEY, exception_dict, now, date_time):
+    class DailyWeather:
+        city = daily_data['cod']
+        country = exception_dict[int(city)]
+        temp = "---"
+        temp_feels = "---"
+        wind = "---"
+        humidity = "---"
+        temp_max = "---"
+        temp_min = "---"
+        city_lon = 34.8
+        city_lat = 32.0833
+        datetime_get = "---"
+        short_desc = "---"
+        desc = "---"
+        icon_data = "bla bla bla"
+        icon = f'static/images/exception_img.png'
+    weekly_source = f'https://api.openweathermap.org/data/2.5/onecall?lat' \
+                    f'={DailyWeather.city_lat}&lon={DailyWeather.city_lon}&units=metric&exclude=current,minutely,hourly,alerts&appid={API_KEY}'
     weekly_data = requests.get(weekly_source).json()
+
     s = str(now.strftime("%d/%m/%Y"))
     print(s)
     date = datetime.strptime(s, '%d/%m/%Y').date()
@@ -61,9 +111,9 @@ def home():
     while i <= 7:
         modified_date = date + timedelta(days=i)
         datetime.strftime(modified_date, '%d/%m/%Y')
-        dict1 = {i: {'day': weekly_data['daily'][i]['temp']['day'], 'night': weekly_data['daily'][i]['temp']['night'],
-                     'weather': weekly_data['daily'][i]['weather'][0]['description'],
-                     'humidity': weekly_data['daily'][i]['humidity'], 'date': modified_date},
+        dict1 = {i: {'day': "---", 'night': "---",
+                     'weather': "---",
+                     'humidity': "---", 'date': modified_date},
                  }
         weather_data.update(dict1)
         i += 1
@@ -72,4 +122,4 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
